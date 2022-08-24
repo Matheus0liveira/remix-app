35bd9cd2f30eac171d8fb3c64721ed21e82afc89
Matheus Oliveira <contato@moliveira.dev>
2022-08-23 21:29:27 -0300

    feat: create admin routes

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,20 +1,30 @@
-import type {
-  ActionFunction,
-  ErrorBoundaryComponent,
-  LoaderFunction,
-} from "@remix-run/node";
+import type { Course } from "@prisma/client";
+import type { ErrorBoundaryComponent, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import type { CatchBoundaryComponent } from "@remix-run/react/dist/routeModules";
+import { db } from "~/utils/db.server";
 
-export const loader: LoaderFunction = ({ params }) => {
-  return {};
+type LoaderData = {
+  courses: Course[];
 };
 
-export const action: ActionFunction = ({ params }) => {
-  return {};
+export const loader: LoaderFunction = async ({ params }) => {
+  const courses = await db.course.findMany();
+  return { courses };
 };
 
 export default function () {
-  return <div>Content is king!</div>;
+  const { courses } = useLoaderData<LoaderData>();
+
+  console.log(courses);
+  return (
+    <div>
+      <h1>Courses</h1>
+      {courses.map((course) => (
+        <h1 key={course.id}>{course.name}</h1>
+      ))}
+    </div>
+  );
 }
 
 export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
@@ -24,3 +34,7 @@ export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
 export const CatchBoundary: CatchBoundaryComponent = () => {
   return <h3>Not Found</h3>;
 };
+
+// export const action: ActionFunction = ({ params }) => {
+//   return {};
+// };
