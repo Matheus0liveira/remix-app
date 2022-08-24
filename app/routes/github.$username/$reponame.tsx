ba17c4bb58f7b87c43fadc85e1bce96d304f7527
Matheus Oliveira <contato@moliveira.dev>
2022-08-24 17:24:19 -0300

    fix: improve ux when not found repo

--- app/routes/github.$username/$reponame.tsx
+++ app/routes/github.$username/$reponame.tsx
@@ -1,5 +1,6 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { NotFound } from "~/components";
 import type { Types } from "~/features/github";
 import { GithubAPI } from "~/features/github";
 import { Commits } from "~/features/github/components/Commits";
@@ -7,13 +8,32 @@ import { Commits } from "~/features/github/components/Commits";
 export const loader: LoaderFunction = async ({
   params,
 }): Promise<Types.Commits.LoaderData> => {
-  return {
-    user: await GithubAPI.getGithubUser(params.username),
-    commits: await GithubAPI.getGithubCommits(params.reponame, params.username),
-  };
+  try {
+    const user = await GithubAPI.getGithubUser(params.username);
+
+    const commits = await GithubAPI.getGithubCommits(
+      params.reponame,
+      params.username
+    );
+
+    return {
+      user,
+      commits,
+    };
+  } catch (error) {
+    throw new Response("Repo not found", { status: 404 });
+  }
 };
 
 export default function () {
   const { commits, user } = useLoaderData<Types.Commits.LoaderData>();
   return <Commits commits={commits} user={user} />;
 }
+
+export const CatchBoundary = () => {
+  return (
+    <div className="p-2">
+      <NotFound message="We couldn'd find a repository with provider ID" />
+    </div>
+  );
+};
