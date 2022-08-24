4b7496ce39136a3e02a5858d4d92bff57b02144d
Matheus Oliveira <contato@moliveira.dev>
2022-08-24 15:03:19 -0300

    feat: add adminAPI

--- app/features/admin/Admin.api.ts
+++ app/features/admin/Admin.api.ts
@@ -4,3 +4,32 @@ import { db } from "~/utils/db.server";
 export const getCourses = (): Promise<Course[]> => {
   return db.course.findMany();
 };
+
+export const saveCourse = (
+  data: Pick<Course, "name" | "description">,
+  id?: string
+): Promise<Course> => {
+  if (id) {
+    return db.course.update({
+      where: { id },
+      data,
+    });
+  }
+  return db.course.create({
+    data,
+  });
+};
+export const getCourse = (id: string): Promise<Course | null> => {
+  return db.course.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+export const deleteCourse = (id: string): Promise<Course> => {
+  return db.course.delete({
+    where: {
+      id,
+    },
+  });
+};
